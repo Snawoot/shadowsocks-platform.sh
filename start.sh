d4f3814e225d112d77c7755861314c4e1fa114e3
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-05-08 19:28:44 +0300

    fix ss parameter quotes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./go-shadowsocks2 \
-	-s "ss://AEAD_CHACHA20_POLY1305:${SS_PASSWORD}@:${PORT}" \
+	-s "ss://AEAD_CHACHA20_POLY1305:${SS_PASSWORD:-MyC001P4ssw0rd}@:${PORT}" \
 	-verbose \
 	-plugin v2ray-plugin \
-	-plugin-opts 'server;path=${V2_PATH:-/v2}'
+	-plugin-opts "server;path=${V2_PATH:-/v2}"
