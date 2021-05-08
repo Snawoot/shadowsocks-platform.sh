83e9b7453d4a29eee7e900ee112509631da85269
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-05-08 21:16:52 +0300

    empty password safeguard

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+[[ "$SS_PASSWORD" ]] || {
+	2>&1 echo "Empty shadowsocks password! Please set SS_PASSWORD environment variable."
+	sleep infinity
+}
+
 ./go-shadowsocks2 \
-	-s "ss://AEAD_CHACHA20_POLY1305:${SS_PASSWORD:-MyC001P4ssw0rd}@:${PORT}" \
+	-s "ss://AEAD_CHACHA20_POLY1305:${SS_PASSWORD}@:${PORT}" \
 	-verbose \
 	-plugin v2ray-plugin \
 	-plugin-opts "server;path=${V2_PATH:-/v2}"
